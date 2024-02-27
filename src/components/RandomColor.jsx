274473bbafaf4d2276677df73f042b40b51fb7e8
ahmed <a.salawu@outlook.com>
2024-02-27 08:09:03 +0000

    Updated the UI

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -20,39 +20,61 @@ const RandomColor = () => {
   };
 
   const handleCreateRGBColor = () => {
-      const r = Math.floor(Math.random() * 255)
-    const g = Math.floor(Math.random() * 255)
-    const b = Math.floor(Math.random() * 255)
-    const rgbColor = `rgb(${r},${g},${b})`
-    const rgbColor2 = `rgb(${utility(255)},${utility(255)},${utility(255)})`
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const rgbColor = `rgb(${r},${g},${b})`;
+    const rgbColor2 = `rgb(${utility(255)},${utility(255)},${utility(255)})`;
 
     console.log(rgbColor);
     console.log(rgbColor2);
     setColor(rgbColor);
-  }
+  };
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        background: color,
-      }}
-    >
-      <div className={styles.wrapper}>
-        <button onClick={() => setColorType("rgb")}>Generate RGB color</button>
-        <button onClick={() => setColorType("hex")}>Generate Hex color </button>
-        <button
-          onClick={
-            colorType === "hex" ? handleCreateHexColor : handleCreateRGBColor
-          }
-        >
-          Generate Random Color
-        </button>
+    <div className={styles.container}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          background: color,
+          boxSizing: "border-box",
+          overflow: "hidden",
+        }}
+      >
+        <div className={styles.wrapper}>
+          <button
+            onClick={() => setColorType("rgb")}
+            className={
+              colorType === "rgb" ? `${styles.active}` : `${styles.deactivated}`
+            }
+          >
+            Generate RGB color
+          </button>
+          <button
+            onClick={() => setColorType("hex")}
+            className={
+              colorType === "hex" ? `${styles.active}` : `${styles.deactivated}`
+            }
+          >
+            Generate Hex color
+          </button>
+          <button
+            onClick={
+              colorType === "hex" ? handleCreateHexColor : handleCreateRGBColor
+            }
+            className={styles.generate}
+          >
+            Generate Random Color
+          </button>
+        </div>
+        <div className={styles.center}>
+            <h1>{colorType === "rgb" ? "RGB Color" : "HEX Color"}</h1>
+            <h1>{color}</h1>
+        </div>
       </div>
-      <div>{color}</div>
     </div>
   );
 };
